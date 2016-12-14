5571f3b889631ef7f93b2d1920712bc488a3ae9b
stoneqi <xiyaozhe@163.com>
2016-12-14 18:29:39 +0800

    学生查询

--- Application/Home/Controller/ReportController.class.php
+++ Application/Home/Controller/ReportController.class.php
@@ -8,10 +8,14 @@
 
 namespace Home\Controller;
 
+use Org\Util\Date;
 use Think\Controller;
 use Home\Service\ReportService;
 
-
+/**
+ * 报表控制器，生成报表方法
+ * @package Home\Controller
+ */
 class ReportController extends Controller
 {
     //显示教师管理视图
@@ -25,19 +29,77 @@ class ReportController extends Controller
         }
     }
 
+    /**
+     * 显示学生信息查询界面
+     */
     public function student_info()
     {
         $this->display('student_info');
     }
 
+    /**
+     * 根据前台Post数据查询学生数据，调用 ReportService 类
+     * 返回 json 格式
+     * {
+     *  code:200,
+     *  message:"",
+     *  data:{
+     *       student_name: ,
+     *       student_id
+     *
+     *      }
+     * }
+     * @return json 学生信息
+     */
     public function getStudentInfo()
     {
-        $startTime = I('post.startTime', 'flase');
-        $endTime = I('post.endTime', 'flase');
-        $studentName = I('post.studentName', '0');
+        /**
+         * 查询开始时间
+         * @var String 默认为0
+         */
+        $startTime = I('post.startTime', '0');
+        /**
+         * 查询结束时间
+         * @var String 默认为0
+         */
+        $endTime = I('post.endTime', '0');
+        /**
+         * 查询学生名字，默认为0，未填写
+         * @var String 默认为0
+         */
+        $studentName = I('post.name', '0');
+        /**
+         * 传入状态字符，0：未选择，1：缺课，2：请假，3：缺课+请假
+         * @var Int 默认为0
+         */
+
         $status = I('post.status', '0');
+
+//        $startTime = \DateTime::createFromFormat('y-m-d',$startTime);
+//        $endTime = \DateTime::createFromFormat('y-m-d',$endTime);
+//        exit();
+        //$startTime =
+
         $reportService = new ReportService();
-        $reportService->getStudentInfo($startTime, $endTime, $studentName, $status);
+        $data = $reportService->getStudentInfo($startTime, $endTime, $studentName, $status);
+        echo json_encode($data);
+        //echo $this->toJson('200','',$data);
+    }
+
+    /**
+     * 对传入的参数进行json转码。
+     *
+     * @param $code
+     * @param $message
+     * @param $data
+     * @return string
+     */
+    public function toJson($code,$message,$data)
+    {
+        $reposed['code'] = $code;
+        $reposed['message'] = $message;
+        $reposed['data'] = $data;
+        return json_encode($reposed);
     }
 
 }
--- Application/Home/Service/ReportService.class.php
+++ Application/Home/Service/ReportService.class.php
@@ -9,10 +9,86 @@
 namespace Home\Service;
 
 use Think\Model;
-class ReportService extends Model
+
+/**
+ * 为 ReportController 提供服务
+ * @package Home\Service
+ */
+class ReportService
 {
+
+    /**
+     * 根据参数返回学生信息。
+     *
+     * @param $startTime
+     * @param $endTime
+     * @param $studentName
+     * @param $status
+     */
     public function getStudentInfo($startTime, $endTime, $studentName, $status)
     {
-        
+        $signInfo = M('sign_info');
+
+        //根据学生名模糊查询学生ID
+        if ($studentName) {
+            $student = M('student');
+            $map['student_name'] = array('like','%'.$studentName.'%');
+            $studentIds = $student->where($map)->select();
+            foreach ($studentIds as $key => $value) {
+                $studentId[] = $value['student_id'];
+            }
+            if ($studentId){
+                $studentSql['sign_info.student_id'] = array('in', $studentId);
+            }else{
+                return "";
+            }
+
+        }
+
+        //查询签到ID
+        if ($startTime or $endTime) {
+            $startTimeSql = $startTime ? "where date >=" . $startTime : "";
+            $endTimeSql = $endTime ? "and date <=" . $startTime : "";
+            $Model = new Model(); // 实例化一个model对象 没有对应任何数据表$Model
+            $signIds = $Model->query("select sign_id from sign " . $startTimeSql . " " . $endTimeSql . ";");
+
+            foreach ($signIds as $key => $value) {
+                $signId[] = $value['sign_id'];
+            }
+            if ($signId){
+                $studentSql['sign_info.sign_id'] = array('in', $signId);
+            }else{
+                return "";
+            }
+        }
+
+
+
+        $status ? $studentSql['sign_info.sign_info_status'] = $status : "";
+        if ($studentSql) {
+            $results = $signInfo->join('student ON sign_info.student_id = student.student_id')->join('sign ON sign_info.sign_id = sign.sign_id');
+            $results = $results->where($studentSql)->select();
+        } else {
+            $results = $signInfo->join('student ON sign_info.student_id = student.student_id')->join('sign ON sign_info.sign_id = sign.sign_id');
+            $results = $signInfo->select();
+        }
+        $teacher = M('teacher');
+        foreach ($results as $key=>$value){
+            $teacher->find($value['teacher_id']);
+            $results[$key]['teacher_name'] = $teacher->teacher_name;
+        }
+
+
+        return $results;
     }
+
+    private function andFilterWhere(array $condition)
+    {
+
+        if ($condition !== []) {
+            $this->andWhere($condition);
+        }
+        return $this;
+    }
+
 }
